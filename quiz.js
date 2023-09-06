e981cc7cec9667e6f61f9f0724cc152a61ff4bb6
Ezio Godoy <eziogodoylopez@gmail.com>
2023-09-06 17:53:52 -0400

    javascript (quiz creation)

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
-var questionsEl = document.querytSelector('.question');
+var questionEl = document.querySelector('.question');
 var answerEl = document.querySelector('.answers');
 var btnStart = document.querySelector('.start');
-var controlsContainer = document.querySelector('control')
+var controlsContainer = document.querySelector('.control')
 var playerInput = document.querySelector('#player')
 var timerEl = document.querySelector('.timer');
 var countEl = document.querySelector('.score')
@@ -10,7 +10,7 @@ var questionNumber = 0;
 var timerCount = 60;
 var scoreCount = 0;
 var setTime2;
-var playersName;
+var playerName;
 var playersList = [];
 var storedList = JSON.parse(localStorage.getItem('playerList'));
 console.log(storedList)
@@ -19,8 +19,40 @@ if (Array.isArray(storedList)) {
     playersList.sort(function (a, b) {
         return b.score - a.score;
     });
-    
-
-
+    playersList.forEach(createRanking); 
 }
 
+var quiz = [{
+        question: "Wich one of this is an html tag?",
+        answers: ["<p>", "<hello>", "<xd>", "<create>"],
+        correct: 0
+    },
+    {
+        question: "Coding language used in M.E.R.N ?",
+        answers: ["Java", "Javascript", "Phyton", "PHP"],
+        correct: 1
+    },
+    {
+        question: "All arrays start on the index?",
+        answers: ["3", "-1", "0", "first"],
+        correct: 2
+    },
+    {
+        question: "Coding language used to style your websites?",
+        answers: ["javascript", "css", "html", "none"],
+        correct: 1
+    }
+];
+
+function gameOver() {
+    timerCount = 0;
+    if (scoreCount > 2) {
+
+        answerEl.innerHTML = `<h1> your score is: ${scoreCount} out of 4 YOU PASSED!!!</h1>`;
+    } else {
+        answerEl.innerHTML = `<h1> your score is: ${scoreCount} out of 4 TRY AGAIN!!!</h1>`;
+
+
+    }
+    controlsContainer.style.display = 'block';
+}
